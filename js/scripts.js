85bf4e314dc18dbcb51eb660524be7104ba5fcef
dcuthell <dcc1447@gmail.com>
2017-07-07 11:29:37 -0700

    change panel ids, add custom js vars and scripts to start branching effects.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
   $("#trackForm").submit(function(event){
+    var name = $("input#inputName").val();
+    var location = $("#inputLocation").val();
+    var size = $("input:radio[name=inputSize]:checked").val();
+    var web = $("input:radio[name=inputWeb]:checked").val();
+    var backend = $("input:radio[name=inputBackend]:checked").val();
+    var popular = $("input:radio[name=inputPopular]:checked").val();
+    var apps = $("input:radio[name=inputApps]:checked").val();
+
+    $("#myName").text(name);
+    $("#myLocation").text(location);
+
     $("#formRow").hide();
     $("#answerRow").show();
     event.preventDefault();
